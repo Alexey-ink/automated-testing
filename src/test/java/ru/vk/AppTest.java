4403929b332357a0f79cdca18b102a963b2809f1
Alexey Shihalev <alesha.shikhalev@mail.ru>
2025-03-17 12:59:19 +0300

    init: initial commit with first unit-test

---
+++ src/test/java/ru/vk/AppTest.java
@@ -0,0 +1,38 @@
+package ru.vk;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName The second home task from VK education
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue(2 + 2*2 == 6);
+    }
+}
